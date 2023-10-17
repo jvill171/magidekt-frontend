b89ab165278b14db3b41cf5d770a412b8893ad2b
jvill171 <juanvillarreal64@yahoo.com>
2023-10-16 20:15:57 -0500

    Added simple Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,18 @@
+
+import "./Loading.scss"
+
+
+const Loading = () =>{
+
+    return(
+        <h1 className="Loading">
+          <i class="ms ms-g"></i>
+          <i class="ms ms-u"></i>
+          <i class="ms ms-r"></i>
+          <i class="ms ms-w"></i>
+          <i class="ms ms-b"></i>
+        </h1>
+    )
+}
+
+export default Loading;
