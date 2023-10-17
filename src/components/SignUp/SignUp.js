56672461748bb4eed887822710ae0d678b6cb9ed
jvill171 <juanvillarreal64@yahoo.com>
2023-10-17 00:33:35 -0500

    Fleshed out SignUp component

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,55 @@
-import "./SignUp.scss"
+import AuthForm from "../AuthForm/AuthForm";
+import MagidektApi from "../../helpers/MagidektApi";
+import formatErrMsg from "../../helpers/formatErrMsg"
 
 const SignUp = () =>{
+    const formAttr = [
+            {
+                label: "Username",
+                name:  "username",
+                initVal: "",
+            },{
+                label: "Password",
+                name:  "password",
+                initVal: "",
+                type: "password",
+            },{
+                label: "Email",
+                name: "email", 
+                initVal: "",
+                type: "email",
+            },
+        ]
 
+        // Signup
+        const submitAction = async(data) =>{
+            let res = await MagidektApi.signUp(data)
+            if(res.message){
+                const labels = {};
+                    for (const field of formAttr) {
+                        labels[field.name] = field.label;
+                    }
+                formatErrMsg(res.message, labels)
+            }
+            return res;
+        }
+        
     return(
-        <div className="Home">
-            <h1>SignUp Component</h1>
-        </div>
+        <>
+            <AuthForm 
+                formTitle="Sign Up"
+                formAttr={formAttr}
+                submitAction={submitAction}
+            />
+            <div>
+                <p>
+                    <b>NOTE</b>:
+                    Username must be unique and cannot be changed after you have chosen it.
+                    <br/>
+                    You will be given the opportunity to update your Display Name however.
+                </p>
+            </div>
+        </>
     )
 }
 
