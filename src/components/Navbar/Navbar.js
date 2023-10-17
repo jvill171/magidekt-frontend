def7ad4ceaf06c53aa64fcac6980287de5fff651
jvill171 <juanvillarreal64@yahoo.com>
2023-10-16 20:17:49 -0500

    Updated Navbar to use UserContext

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,53 @@
+import { useContext } from "react";
+import UserContext from "../../context/UserContext";
 import "./Navbar.scss"
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () =>{
+    
+    const { user, updateUser } = useContext(UserContext)
+    
+    const handleSignout = () =>{
+        localStorage.removeItem(`_token`)
+        updateUser();
+    }
 
     return(
-        <nav className="navbar">
-            <div className="logo">
-                <Link to="/"><span>Magi</span>dekt</Link>
+        <nav className="navbar-fade">
+            <div className="navbar"> 
+                <div className="logo">
+                    <Link to="/"><span>Magi</span>dekt</Link>
+                </div>
+                <ul className="nav-NavLinks">
+                    {user ?
+                    <>
+                        <li>
+                            <NavLink to="/profile" >
+                                Profile
+                            </NavLink>
+                        </li>
+                        <li>
+                            <div className="signout" onClick={handleSignout}>
+                                Signout
+                            </div>
+                        </li>
+                    </>
+                    :
+                    <>
+                        <li>
+                            <NavLink to="/login" >
+                                Login
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/signup" >
+                                Signup
+                            </NavLink>
+                        </li>
+                    </>
+                    }
+                </ul>
             </div>
-            <ul className="nav-NavLinks">
-                <li>
-                    <NavLink to="/login" >
-                        Login
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/signup" >
-                        Signup
-                    </NavLink>
-                </li>
-            </ul>
         </nav>
     )
 }
