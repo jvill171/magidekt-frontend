8c9fc7cb70dcc26fe38c9820ce10bff67e2fe6af
jvill171 <juanvillarreal64@yahoo.com>
2023-10-19 23:10:14 -0500

    Created component to show a specific user's decks

--- src/components/DeckList/DeckList.js
+++ src/components/DeckList/DeckList.js
@@ -1,10 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import UserContext from "../../context/UserContext";
+import MagidektApi from "../../helpers/MagidektApi";
+
 import "./DeckList.scss"
+import DeckListItem from "../DeckListItem/DeckListItem";
+import { useParams } from "react-router-dom";
+import Loading from "../Loading/Loading";
 
 const DeckList = () =>{
 
+    const [userDecks, setUserDecks] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const { user } = useContext(UserContext)
+    
+    useEffect(()=>{
+        async function getUserDecks(){
+            const decksRes = await MagidektApi.getUserDecks(user);
+            setUserDecks([...decksRes])
+        }
+        getUserDecks();
+        setIsLoading(false)
+    }, [])
+
     return(
         <div className="DeckList">
-            <h1>DeckList Component</h1>
+            <h1>{user}'s Decks</h1>
+            {isLoading 
+                ?
+                <>
+                    <br/>
+                    <Loading showText={true} breakCount={2}/>
+                </>
+                :
+                <>
+                    
+                    <div className="deck-list">
+                        {userDecks && userDecks.map(d=>
+                            <DeckListItem className="deck-item" key={`deck-${d.id}`} data={d} />)
+                        }
+                    </div>
+                </>
+            }
         </div>
     )
 }
