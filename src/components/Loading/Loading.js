cb984b4c21f64bb7fa9e44db22e9c6ad216d2635
jvill171 <juanvillarreal64@yahoo.com>
2023-10-19 23:05:58 -0500

    Added option to add 'Loading...' text & breaks

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,16 +2,25 @@
 import "./Loading.scss"
 
 
-const Loading = () =>{
+const Loading = ({showText = false, breakCount = 0}) =>{
+
+  const lineBreaks = [];
+  for (let i = 0; i < breakCount; i++) {
+    lineBreaks.push(<br key={i} />);
+  }
 
     return(
+      <>
         <h1 className="Loading">
+        {showText && <span>Loading... </span>}
+        {lineBreaks}
           <i className="ms ms-g"></i>
           <i className="ms ms-u"></i>
           <i className="ms ms-r"></i>
           <i className="ms ms-w"></i>
           <i className="ms ms-b"></i>
         </h1>
+      </>
     )
 }
 
