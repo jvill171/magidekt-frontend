91d4fe261023c18706e3512bac521ab2f6bdb6bd
jvill171 <juanvillarreal64@yahoo.com>
2023-10-28 00:24:04 -0500

    Created base components to display cards (Mainboard) and search for cards (SearchScryfall)

---
+++ src/components/Mainboard/Mainboard.js
@@ -0,0 +1,13 @@
+
+import "./Mainboard.scss"
+
+
+const Mainboard = () =>{
+    return(
+      <>
+        <h1 className="Mainboard">Mainboard component</h1>
+      </>
+    )
+}
+
+export default Mainboard;
---
+++ src/components/SearchScryfall/SearchScryfall.js
@@ -0,0 +1,13 @@
+
+import "./SearchScryfall.scss"
+
+
+const SearchScryfall = () =>{
+    return(
+      <>
+        <h1 className="SearchScryfall">SearchScryfall component</h1>
+      </>
+    )
+}
+
+export default SearchScryfall;
