b1afd059d8014e36c2edaf721c30abab2f9b288b
jvill171 <juanvillarreal64@yahoo.com>
2023-10-29 21:44:13 -0500

    Added 5-Mana SVG & small code clean up

--- src/components/IconMTG/IconMTG.js
+++ src/components/IconMTG/IconMTG.js
@@ -5,7 +5,7 @@ import "./IconMTG.scss"
 const IconMTG = ({manaData, spaced = true}) =>{
 
     function genIcon(){
-      const cleanName = "mana-" + manaData.replace(/[{}\/]/g, '');
+      const cleanName = "mana-" + manaData.replace(/[{}/]/g, '');
 
       return <span className={`mana ${cleanName}`}></span>
     }
