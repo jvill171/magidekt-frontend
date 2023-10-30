50514ad458ad0a2ce98947d031d6d98d8ccba65e
jvill171 <juanvillarreal64@yahoo.com>
2023-10-29 21:53:49 -0500

    Created empty base component for CardInfo

---
+++ src/components/CardInfo/CardInfo.js
@@ -0,0 +1,14 @@
+
+import "./CardInfo.scss"
+
+
+const CardInfo = () =>{
+
+    return(
+      <>
+        <h1 className="CardInfo">CardInfo Component</h1>
+      </>
+    )
+}
+
+export default CardInfo;
