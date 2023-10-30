12e2b9b54ebc174de35bbfbf37699b3e4df59727
jvill171 <juanvillarreal64@yahoo.com>
2023-10-29 22:09:47 -0500

    Created helper function to call Scryfall

--- src/helpers/ScryfallApi.js
+++ src/helpers/ScryfallApi.js
@@ -5,6 +5,10 @@ const BASE_URL = "https://api.scryfall.com";
 
 class Scryfall {
 
+    /** request()
+     * 
+     * Constructs & makes the API call to Scryfall API
+    */
     static async request(endpoint, data = {}, method = "get") {
         console.debug("API Call:", endpoint, data, method);
 
@@ -15,7 +19,7 @@ class Scryfall {
 
         try {
             // Uncomment line below for more detailed debug
-            // console.debug(`MAKING API CALL\n`, method, url, `\ndata:`, data)
+            console.debug(`MAKING API CALL\n`, method, url, `\ndata:`, data, `\nparams:`, params)
             return await axios({ url, method, data, params})
         } catch (err) {
           console.error("API Error:", err.response);
@@ -27,6 +31,11 @@ class Scryfall {
         }
     }
 
+    /** getRandom()
+     * 
+     * Retreives a random card from Scryfall that meets the criteria "q"
+     *      Returns 1 random Scryfall card
+     */
     static async getRandom(q){
         try{
             const data = { q }
@@ -38,6 +47,13 @@ class Scryfall {
         }
     }
 
+    /** search()
+     * 
+     * Queries the Scryfall API for all cards that meet the criteria "q"
+     * retreives page "page" of results
+     * 
+     * Returns up to 175 Scryfall card per page/call
+     */
     static async search(q, page){
         try{
             const data = { q, page }
@@ -49,10 +65,38 @@ class Scryfall {
         }
     }
 
+    /** getByURL()
+     * 
+     * Retreives the result of an API call to the specified "targetURL"
+     * 
+     * This function can be useful alongside the above "search()" method
+     * because "search()" also returns the next page uri, if it exists 
+     * 
+     */
     static async getByURL( targetURL ){
         try{
             const endpoint = targetURL.slice(BASE_URL.length + 1)
             let res = await this.request(endpoint)
+            return res.data
+        }catch(err){
+            console.error(err)
+            return err;
+        }
+    }
+
+    /** resolveUIDs()
+     *      data:[{cardId, quantity}, ...] => <Scryfall Response>
+     * 
+     * Retreives all specified cards from Scryfall, based on their UIDs
+     * 
+     * Returns all found cards Data
+     * 
+     */
+    static async resolveUIDs( data ){
+        
+        const identifiers = data.map(c => ({"id": c.cardId}))
+        try{
+            let res = await this.request(`cards/collection`, {identifiers}, "post")
             return res.data
         }catch(err){
             console.error(err)
