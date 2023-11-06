ba7d39f5f7762836a19557b901f1669d27fa8b1f
jvill171 <juanvillarreal64@yahoo.com>
2023-11-05 23:58:53 -0600

    Created a form for processing deltion of Decks & profiles

---
+++ src/components/DeleteForm/DeleteForm.js
@@ -0,0 +1,37 @@
+
+import { useState } from "react"
+import "./DeleteForm.scss"
+import Magidekt from "../../helpers/MagidektApi"
+
+
+const DeleteForm = ({toDelete = "???", deleteAction}) =>{
+
+
+    const [verifyDelTxt, setVerifyDelTxt ] = useState("")
+    const toMatch = "Magidekt!"
+
+    const handleDelInput = (e) => {
+        setVerifyDelTxt(e.target.value);
+    };
+
+    const handleDelete = async (e)=>{
+        e.preventDefault()
+        await deleteAction();
+    }
+
+    return(
+      <form className="DeleteForm">
+        <h3>To delete your {toDelete}, type the following:</h3>
+        <p>Magidekt!</p>
+        <input type="text" onChange={handleDelInput} value={verifyDelTxt}/>
+        <br/>
+        <button
+            className="delete-btn"
+            onClick={handleDelete}
+            disabled={toMatch !== verifyDelTxt}
+        > Delete {toDelete} </button>
+      </form>
+    )
+}
+
+export default DeleteForm;
