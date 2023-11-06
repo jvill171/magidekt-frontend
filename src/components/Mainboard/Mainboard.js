19111ab36cfe807241514eb4b7029f2d189d438c
jvill171 <juanvillarreal64@yahoo.com>
2023-11-06 00:21:29 -0600

    Near complete refactor for code optimization

--- src/components/Mainboard/Mainboard.js
+++ src/components/Mainboard/Mainboard.js
@@ -1,74 +1,83 @@
 
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import DeckCategoryCards from "../DeckCategoryCards/DeckCategoryCards"
 import "./Mainboard.scss"
 import makeTitleCase from "../../helpers/makeTitleCase"
+import Loading from "../Loading/Loading";
 
+const Mainboard = ({deckCards, updateCount, dataLoaded}) =>{
 
-const Mainboard = ({deckCards, updateCount}) =>{
+  const INIT_CATEGORIES = {
+    Planeswalker: [], Token: [], Creature: [], Land: [], Artifact: [],
+    Enchantment: [], Sorcery: [], Instant: [], Scheme: [], Other: [],
+  }
 
-    const [cardCategorys, setCardCategories] = useState([])
-    const allCats = [
-      "Planeswalker","Token","Creature","Land","Artifact",
-      "Enchantment","Sorcery","Instant","Scheme", "Other" ]
-
-    function determineCategory(card){
-      let cTypeLine = (card.type_line || card.card_faces[0].type_line).toLowerCase();
-
-      for(let i = 0; i< allCats.length; i++){
-        if(cTypeLine.includes(allCats[i].toLowerCase())){
-          return makeTitleCase(allCats[i]);
-        }
+  const [cardCats, setCardCats] = useState(INIT_CATEGORIES)
+  
+  const allCats = [ "Planeswalker","Token","Creature","Land","Artifact", "Enchantment","Sorcery","Instant","Scheme", "Other" ]
+  const catOrder = [ "Land", "Creature", "Artifact", "Planeswalker", "Enchantment", "Sorcery", "Instant", "Token","Scheme", "Other" ]
+  
+  function categorizeData(data, allCategories) {
+    return data.reduce((result, card) => {
+      if(card.magidekt.qty > 0){
+        const category = determineCategory(card);
+        allCategories[category].push(card);
       }
-      return "Other";
+      return result;
+    }, allCategories);
+  }
+
+  function determineCategory(card){
+    const cTypeLine = (card.type_line || card.card_faces[0].type_line).toLowerCase();
+
+    for(let i = 0; i< allCats.length; i++){
+      if(cTypeLine.includes(allCats[i].toLowerCase())){
+        return makeTitleCase(allCats[i]);
+      }
     }
+    return "Other";
+  }
 
-    function filterByCategory(category){
-      return deckCards.filter(card => determineCategory(card) === category)
+  useEffect(()=>{
+    if(dataLoaded){
+      const sortedData = categorizeData(deckCards, INIT_CATEGORIES)
+      setCardCats(sortedData)
     }
+  },[deckCards, dataLoaded])
 
-    // Set cards into their categories
-    useEffect(()=>{
-      const uniqueTypes = new Set();
-        deckCards.forEach( card => {
-          const foundType = determineCategory(card);
-          uniqueTypes.add(foundType);
-      })
-      
-      const catOrder = [
-        "Land", "Creature", "Artifact", "Planeswalker", "Enchantment",
-        "Sorcery", "Instant", "Token","Scheme", "Other" ]
-
-      const orderedUnique = catOrder.filter(category => uniqueTypes.has(category))
-      setCardCategories( orderedUnique )
-    },[deckCards])
-
-    return(
-      <div className="card-category-container">
-
-        {deckCards && cardCategorys.map((categ, catIdx) =>
-          <div style={{zIndex: catIdx}} key={`category-${categ}`} className="card-category">
-            
-            <div className="title-category">
-              {categ !== "Other" && <i className={`ms ms-${categ.toLowerCase()} title-icon`}></i>}
-                <p className="title-text">{categ}</p>
-            </div>
-            <ul className="card-list">
-              {filterByCategory(categ).map((c) => (
-                c.magidekt_qty > 0 &&
-                  (<li key={`card-${c.id}`} className="card">
-                    <DeckCategoryCards
-                      cardData={c}
-                      updateCount={updateCount}
-                    />
-                  </li>)
-              ))}
-            </ul>
+  return(
+    <>
+      { !dataLoaded
+        ? <Loading showText={true} breakCount={2}/>
+        : <div className="card-category-container">
+            {deckCards && catOrder.map((categ, catIdx) =>
+              cardCats[categ].length > 0 && (
+                <div style={{zIndex: catIdx}}  className="card-category" key={`category-${categ}`}>
+                  
+                  <div className="title-category">
+                    {categ !== "Other" &&
+                      <i className={`ms ms-${categ.toLowerCase()} title-icon`}></i>
+                    }
+                      <p className="title-text">{categ}</p>
+                  </div>
+                  <ul className="card-list">
+                    {cardCats[categ].map(c =>(
+                      // c.magidekt.qty > 0 &&
+                      (
+                        <li key={`card-${c.id}`} className="card">
+                          <DeckCategoryCards cardData={c} updateCount={updateCount}/>
+                        </li>
+                      )
+                    ))}
+                  </ul>
+                </div>
+              )
+            )}
           </div>
-        )}
-
-      </div>
-    )
+      }
+    </>
+  )
 }
 
 export default Mainboard;
+
