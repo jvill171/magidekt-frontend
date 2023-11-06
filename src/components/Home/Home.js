4341c513bcc27b5f99fb1bca1602ad97a649a034
jvill171 <juanvillarreal64@yahoo.com>
2023-11-06 00:08:12 -0600

    Added some user context to Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,31 @@
 import "./Home.scss"
 import { NavLink } from "react-router-dom";
-
+import { useContext } from 'react';
+import UserContext from '../../context/UserContext';
 
 const Home = () =>{
+    const {user} = useContext(UserContext)
 
     return(
         <div className="Home">
-            <h1>Magidekt</h1>
-            <div className="auth-buttons">
-                <NavLink to="/login" >
-                    <button>Login</button>
-                </NavLink>
-                <NavLink to="/signup" >
-                    <button>Sign Up</button>
-                </NavLink>
-            </div>
+            { user
+                ? <div>
+                    <h2>Welcome {user}</h2>
+                    <p>To view your created decks, navigate to <NavLink to={`/decks/${user}`} style={{fontWeight:"bold"}}>Decks</NavLink> on the top right</p>
+                  </div>
+                : <>
+                    <h1>Magidekt</h1>
+                    <div className="auth-buttons">
+                        <NavLink to="/login" >
+                            <button>Login</button>
+                        </NavLink>
+                        <NavLink to="/signup" >
+                            <button>Sign Up</button>
+                        </NavLink>
+                    </div>
+                  </>
+            }
+            
 
         </div>
     )
