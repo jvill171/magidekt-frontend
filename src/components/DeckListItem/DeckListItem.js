4ac632ff5c22f64293afd62fc13f4d56c77c378b
jvill171 <juanvillarreal64@yahoo.com>
2023-11-06 00:17:49 -0600

    Minor code refactor & using new helpers/components

--- src/components/DeckListItem/DeckListItem.js
+++ src/components/DeckListItem/DeckListItem.js
@@ -3,6 +3,8 @@ import UserContext from "../../context/UserContext";
 import { NavLink } from "react-router-dom";
 import "./DeckListItem.scss"
 import { identityToMana } from "../../helpers/toMana";
+import makeTitleCase from "../../helpers/makeTitleCase";
+import IconMTG from "../IconMTG/IconMTG";
 
 const DeckListItem = ({data, showOwner = true}) =>{
     const { user } = useContext(UserContext)
@@ -17,20 +19,18 @@ const DeckListItem = ({data, showOwner = true}) =>{
             cardCount } = data;
 
     // Capitalize first letter
-    const upFormat = format[0].toUpperCase() + format.slice(1);
+    const upFormat = makeTitleCase(format);
     // Generate mana icons
     const colorIcons = identityToMana(colorIdentity)
 
     return(
         <NavLink to={`/decks/${user}/${id}`} className="DeckListItem decklist-item">
 
-            <p>{`/decks/${user}/${id}`}</p>
-
             <h3 className="deck-name">{deckName}</h3>
             <p className="deck-identity">
                 {cardCount > 0 
                     ? colorIcons
-                    : <span className="ms no-mana"></span>}
+                    : <IconMTG manaData={"C"} spaced={false}/>}
             </p>
             <ul>
                 {showOwner &&
@@ -38,25 +38,24 @@ const DeckListItem = ({data, showOwner = true}) =>{
                         <b>Owner:</b> {deckOwner}
                     </li>
                 }
-                <li>
+                <li className={`id`}>
                     <b>Deck ID:</b> {id}
                 </li>
-                <li>
+                <li className={`cards`}>
                     <b>Cards:</b> {cardCount}
                 </li>
-                <li>
+                <li className={`format`}>
                     <b>Format:</b> {upFormat}
                 </li>
-                <li>
+                <li className={`tags`}>
                     <b>Tags:</b> {tags && tags.length > 0
                         ? tags.join(', ')
                         : ''}
                 </li>
-                {description && 
-                    <li>
-                        <b>Desc:</b> {description}
-                    </li>
-                }
+                <li className={`description`}>
+                    <b>Description:</b> 
+                        <p>{description}</p>
+                </li>
             </ul>
         </NavLink>
     )
